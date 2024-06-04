7938c84642563acf17b17cdaebb68fda8045156b
vteeev <153308819+vteeev@users.noreply.github.com>
2024-06-04 20:56:02 +0200

    Stylesheet change, operations

--- FancyCalculator.h
+++ FancyCalculator.h
@@ -1,6 +1,8 @@
 ﻿
 
 #include <QtWidgets/QMainWindow>
+#include <string>
+
 #include "ui_FancyCalculator.h"
 #include "Operation.h"
 
@@ -21,7 +23,7 @@ QMessageBox: Komunikaty o błędach.
 stuff TODO
 
 - zapisywanie / odczytywanie historii do pliku
-- uzyc string / vector (STL; smart_pointers???) TODO: think on that 
+- uzyc string / vector (STL; smart_pointers???) TODO: think on that
 - throw_exception -> QT messhage box eror (dzielenie przez 0)
 - ogarnac jak uzywac polimorizmu i takich inncyh dla liczb etc...
 
@@ -30,7 +32,7 @@ stuff TODO
 /*
 ideas:
 
-- zmiany kolorkow / zapisywanie ich 
+- zmiany kolorkow / zapisywanie ich
 - dodanie nowych operacji (np. potegowanie)
 - dodanie nowych trybow (narazie jebac nie dodajemy za duzo myslenia)
 
@@ -45,11 +47,39 @@ public:
     FancyCalculator(QWidget* parent = nullptr);
     ~FancyCalculator();
 
+private:
+    QStringList texts;
+    QString operation_char;
+    QString variable1;
+    QString variable2;
+    bool isChecked = false;
+
 private slots:
+  
     void showPage1();
+    //void showPage2();
+    //void showPage3();
 
 private:
     void wykonajOperacje(const Operation& operacja, double a, double b);
+    QString processText();
+    void cin_operation_char(QChar b);
+    void showNumber(QString i);
+
+    void equal_operation();
+    void plus_operation();
+    void multiplication_operation();
+    void minus_operation();
+    void devide_operation();
+    void power_1x_operation();
+    void power_2_operation();
+
+    void sign_reverse_operation();
+    void delet_operation();
+    void clearText();
+
+    void onBtnToggled();
+
 
 private:
     Ui::FancyCalculatorClass ui;
--- Operation.cpp
+++ Operation.cpp
@@ -1,4 +1,5 @@
 #include "Operation.h"
+#include "FancyCalculator.h"
 #include <stdexcept>
 
 class Dodawanie : public Operation {
@@ -32,3 +33,89 @@ public:
         return a / b;
     }
 };
+void FancyCalculator::equal_operation() {
+    
+    bool ok1, ok2;
+    double wynik = 0.0;
+    double var1 = variable1.toDouble(&ok1);
+    if (operation_char == 'r') {
+        var1 *= -1.0;
+        QString wynik2 = QString::number(var1);
+        showNumber(wynik2);
+        return;
+    }
+    else if (operation_char == '1') {
+        var1 =1/var1;
+        QString wynik2 = QString::number(var1);
+        showNumber(wynik2);
+        return;
+    }
+    else if (operation_char == '2') {
+        var1 *= var1;
+        QString wynik2 = QString::number(var1);
+        showNumber(wynik2);
+        return;
+    }
+
+    variable2 = processText();
+    clearText();
+    double var2 = variable2.toDouble(&ok2);
+    if (!ok1 || !ok2) {
+        qDebug() << "Error: Invalid input for numerical operation";
+        return;
+    }
+    if (operation_char == '+') {
+        wynik = var1 + var2;
+    }
+    else if (operation_char == '-') {
+        wynik = var1 - var2;
+    }
+    else if (operation_char == '/') {
+        wynik = var1 / var2;
+    }
+    else if (operation_char == '*') {
+        wynik = var1 * var2;
+    }
+    else {
+        qDebug() << "Error: Unsupported operation";
+        return;
+    }
+    QString wynik2 = QString::number(wynik);
+    showNumber(wynik2);
+}
+void FancyCalculator::plus_operation() {
+    cin_operation_char('+');
+    variable1 = processText();
+    clearText();
+}
+void FancyCalculator::minus_operation() {
+    cin_operation_char('-');
+    variable1 = processText();
+    clearText();
+}
+void FancyCalculator::devide_operation() {
+    cin_operation_char('/');
+    variable1 = processText();
+    clearText();
+}
+void FancyCalculator::multiplication_operation() {
+    cin_operation_char('*');
+    variable1 = processText();
+    clearText();
+}
+void FancyCalculator::sign_reverse_operation() {
+    cin_operation_char('r');
+    variable1 = processText();
+    
+    clearText();
+}
+void FancyCalculator::power_1x_operation() {
+    cin_operation_char('1');
+    variable1 = processText();
+    clearText();
+}
+void FancyCalculator::power_2_operation() {
+    cin_operation_char('2');
+    variable1 = processText();
+    clearText();
+}
