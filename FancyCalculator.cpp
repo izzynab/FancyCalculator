3f8c53f3c2690fb2d4f031899c34f015d9fd9f7c
izzynab <tomek_2003@interia.pl>
2024-06-09 13:22:57 +0200

    refactored

--- FancyCalculator.cpp
+++ FancyCalculator.cpp
@@ -1,135 +1,150 @@
-﻿
-#include "FancyCalculator.h"
+﻿#include "FancyCalculator.h"
 #include "ui_FancyCalculator.h"
 #include "Operation.h"
 #include <QApplication>
 #include <QCheckBox>
 #include <QWidget>
+#include <QMessageBox>
+#include <stack>
 
 FancyCalculator::FancyCalculator(QWidget* parent)
-    : QMainWindow(parent)
+    : QMainWindow(parent), currentOperation(nullptr), operationLoaded(false), firstNumberLoaded(false), isChecked(false)
 {
     ui.setupUi(this);
 
     // Starting page (default calcualtor)
     ui.stackedWidget->setCurrentIndex(0);
 
-    connect(ui.programmerModeButton, &QPushButton::clicked, this, &FancyCalculator::showPage1);
-    //connect(ui.basicModeButton, &QPushButton::clicked, this, &FancyCalculator::showPage2);
-    //connect(ui.pushButton_55, &QPushButton::clicked, this, &FancyCalculator::showPage3);
-
     connect(ui.Btn_equal, &QPushButton::clicked, this, &FancyCalculator::equal_operation);
-    connect(ui.Btn_plus, &QPushButton::clicked, this, &FancyCalculator::plus_operation);
-    connect(ui.Btn_minus, &QPushButton::clicked, this, &FancyCalculator::minus_operation);
-    connect(ui.Btn_division, &QPushButton::clicked, this, &FancyCalculator::devide_operation);
-    connect(ui.Btn_multiplication, &QPushButton::clicked, this, &FancyCalculator::multiplication_operation);
+    connect(ui.Btn_plus, &QPushButton::clicked, this, &FancyCalculator::operation<Addition>);
+    connect(ui.Btn_minus, &QPushButton::clicked, this, &FancyCalculator::operation<Subtraction>);
+    connect(ui.Btn_division, &QPushButton::clicked, this, &FancyCalculator::operation<Division>);
+    connect(ui.Btn_multiplication, &QPushButton::clicked, this, &FancyCalculator::operation<Multiplication>);
     connect(ui.Btn_C, &QPushButton::clicked, this, &FancyCalculator::clearText);
     connect(ui.Btn_delet, &QPushButton::clicked, this, &FancyCalculator::delet_operation);
-    connect(ui.Btn_reverse, &QPushButton::clicked, this, &FancyCalculator::sign_reverse_operation);
-    connect(ui.Btn_power_2, &QPushButton::clicked, this, &FancyCalculator::power_2_operation);
-    connect(ui.Btn_1x, &QPushButton::clicked, this, &FancyCalculator::power_1x_operation);
 
-    connect(ui.Btn_Pi, &QPushButton::clicked, [=]() { showNumber("3.1415926535897932384626433832"); });
-    connect(ui.Btn_e, &QPushButton::clicked, [=]() { showNumber("2.7182818284590452353602874713"); });
-    connect(ui.Btn_dot, &QPushButton::clicked, [=]() { showNumber("."); });
-    connect(ui.Btn0, &QPushButton::clicked, [=]() { showNumber("0"); });
-    connect(ui.Btn1, &QPushButton::clicked, [=]() { showNumber("1"); });
-    connect(ui.Btn2, &QPushButton::clicked, [=]() { showNumber("2"); });
-    connect(ui.Btn3, &QPushButton::clicked, [=]() { showNumber("3"); });
-    connect(ui.Btn4, &QPushButton::clicked, [=]() { showNumber("4"); });
-    connect(ui.Btn5, &QPushButton::clicked, [=]() { showNumber("5"); });
-    connect(ui.Btn6, &QPushButton::clicked, [=]() { showNumber("6"); });
-    connect(ui.Btn7, &QPushButton::clicked, [=]() { showNumber("7"); });
-    connect(ui.Btn8, &QPushButton::clicked, [=]() { showNumber("8"); });
-    connect(ui.Btn9, &QPushButton::clicked, [=]() { showNumber("9"); });
-    //connect(ui.Btn_plus, &QPushButton::clicked, [=]() { cin_operation_char('+'), & FancyCalculator::plus_operation; });
+    connect(ui.Btn_dot, &QPushButton::clicked, [=]() { loadNumberPart("."); });
+    connect(ui.Btn0, &QPushButton::clicked, [=]() { loadNumberPart("0"); });
+    connect(ui.Btn1, &QPushButton::clicked, [=]() { loadNumberPart("1"); });
+    connect(ui.Btn2, &QPushButton::clicked, [=]() { loadNumberPart("2"); });
+    connect(ui.Btn3, &QPushButton::clicked, [=]() { loadNumberPart("3"); });
+    connect(ui.Btn4, &QPushButton::clicked, [=]() { loadNumberPart("4"); });
+    connect(ui.Btn5, &QPushButton::clicked, [=]() { loadNumberPart("5"); });
+    connect(ui.Btn6, &QPushButton::clicked, [=]() { loadNumberPart("6"); });
+    connect(ui.Btn7, &QPushButton::clicked, [=]() { loadNumberPart("7"); });
+    connect(ui.Btn8, &QPushButton::clicked, [=]() { loadNumberPart("8"); });
+    connect(ui.Btn9, &QPushButton::clicked, [=]() { loadNumberPart("9"); });
 
     connect(ui.Btn_mode, &QPushButton::clicked, this, &FancyCalculator::onBtnToggled);
-
 }
 
-FancyCalculator::~FancyCalculator()
-{
+FancyCalculator::~FancyCalculator() {
+    delete currentOperation;
 }
 
-void FancyCalculator::showPage1()
-{
-    ui.stackedWidget->setCurrentIndex(0); 
+void FancyCalculator::loadNumberPart(const QString& part) {
+    currentNumber += part;
+    currentText = currentNumber;
+    showCurrentText();
 }
 
-
-void FancyCalculator::wykonajOperacje(const Operation& operacja, double a, double b)
-{
-    double value1 = variable1.toDouble();
-    double value2 = variable2.toDouble();
-    a = value1;
-    b = value2;
-    try
-    {
-        double wynik = operacja.wykonaj(a, b);
-        // TODO: print the result
-        //std::cout << "Wynik: " << wynik << std::endl;
+void FancyCalculator::delet_operation() {
+    if (operationLoaded) {
+        currentOperation = nullptr;
+        operationLoaded = false;
     }
-    catch (const std::exception& e)
-    {
-        // TODO: here we can use the QT message box 
-        // create class for handling any type of errors (instead of the std::cerr)
-        //std::cerr << "Błąd: " << e.what() << std::endl;
+    else {
+        currentNumber.chop(1);
     }
+    currentText.chop(1);
+    showCurrentText();
 }
-QString FancyCalculator::processText()
-{
-    // Wczytaj tekst z QTextEdit
-    QString text = ui.textEdit->toPlainText();
-    //ui.label->setText(text);
-    return text;
-}
-
-void FancyCalculator::delet_operation(){
-    QString text = processText();
-    text.chop(1); // Usuwa ostatni znak
-    clearText();
-    showNumber(text);
-}
-
-void FancyCalculator::showNumber(QString i) {
-
-    FancyCalculator::texts << QString(i);
-    QString finalText = texts.join("");
-    ui.textEdit->setPlainText(finalText);
-}
-
-void FancyCalculator::cin_operation_char(QChar b) {
-
-    operation_char=b;
-}
-
 
 void FancyCalculator::clearText() {
-
-    ui.textEdit->clear();
-    texts.clear();
+    currentText.clear();
+    currentNumber.clear();
+    firstNumber.clear();
+    delete currentOperation;
+    currentOperation = nullptr;
+    operationLoaded = false;
+    firstNumberLoaded = false;
+    showCurrentText();
 }
 
-void FancyCalculator::onBtnToggled()
-{
-    isChecked = !isChecked;
-    if (isChecked) {
-        // Ustaw styl dla zaznaczonego checkboxa
-        ui.widget->setStyleSheet("QWidget { background-color:white; color:black }QWidget{ background - color:white; color:black }  QCheckBox { color: blue; }");
-        ui.textEdit->setStyleSheet("QTextEdit {color: black;font-size: 32px;    border:0px,solid,white;}");
-        ui.widget_2->setStyleSheet("QWidget { background-color:white; color:black }");
-        ui.centralWidget->setStyleSheet("QWidget{ margin:0; background-color: white;   }");
-        ui.stackedWidget_2->setStyleSheet("QWidget{ background-color: white;   }");
+void FancyCalculator::showCurrentText() {
+    ui.textEdit->setPlainText(currentText);
 }
-    else {
-        // Resetuj styl
-        ui.centralWidget->setStyleSheet("QWidget{ margin:0; background - color: rgb(24, 24, 24)   }");
-        ui.widget->setStyleSheet("QWidget{ background - color: rgb(24, 24, 24);}");
-        ui.stackedWidget_2->setStyleSheet("QWidget{ background - color: rgb(24, 24, 24);   }");
-        ui.textEdit->setStyleSheet("QTextEdit {background-color: rgb(24, 24, 24);color: white;font-size:32px;border:0px,solid,black;}");
-        ui.widget_2->setStyleSheet("QWidget{ background - color: rgb(24, 24, 24);}");
+
+void FancyCalculator::addOperatorToText() {
+    if (currentOperation) {
+        currentText += currentOperation->getOperator();
     }
 }
 
+template<typename T>
+void FancyCalculator::operation() {
+    if (operationLoaded) {
+        QMessageBox::warning(this, "Błąd", "Operacja już została wybrana.");
+        return;
+    }
+    if (currentNumber.isEmpty()) {
+        QMessageBox::warning(this, "Błąd", "Proszę wprowadzić liczbę przed operacją.");
+        return;
+    }
+    firstNumber = currentNumber;
+    currentNumber.clear();
+    currentOperation = new T();
+    operationLoaded = true;
+    firstNumberLoaded = true;
+    addOperatorToText();
+    showCurrentText();
+}
 
+void FancyCalculator::equal_operation() {
+    if (!operationLoaded || currentNumber.isEmpty()) {
+        QMessageBox::warning(this, "Błąd", "Proszę wprowadzić dwie liczby i jedną operację.");
+        return;
+    }
+
+    bool ok1, ok2;
+    double firstNumberVal = firstNumber.toDouble(&ok1);
+    double secondNumberVal = currentNumber.toDouble(&ok2);
+
+    if (!ok1 || !ok2) {
+        QMessageBox::warning(this, "Błąd", "Niepoprawna liczba.");
+        return;
+    }
+
+    try {
+        double result = currentOperation->execute(firstNumberVal, secondNumberVal);
+        showNumber(QString::number(result));
+    }
+    catch (const std::exception& e) {
+        QMessageBox::warning(this, "Błąd", e.what());
+    }
+
+    delete currentOperation;
+    currentOperation = nullptr;
+    operationLoaded = false;
+    firstNumberLoaded = false;
+    currentNumber.clear();
+    firstNumber.clear();
+}
+
+void FancyCalculator::onBtnToggled() {
+    isChecked = !isChecked;
+    if (isChecked) {
+        ui.widget->setStyleSheet("QWidget { background-color:white; color:black }");
+        ui.textEdit->setStyleSheet("QTextEdit {color: black;font-size: 32px;}");
+        ui.centralWidget->setStyleSheet("QWidget { background-color:white; }");
+    }
+    else {
+        ui.centralWidget->setStyleSheet("QWidget { background-color: rgb(24, 24, 24) }");
+        ui.textEdit->setStyleSheet("QTextEdit {background-color: rgb(24, 24, 24);color: white;font-size:32px;}");
+    }
+}
+
+void FancyCalculator::showNumber(const QString& number) {
+    currentText = number;
+    showCurrentText();
+}
--- Operation.h
+++ Operation.h
@@ -1,6 +1,60 @@
-#pragma once
+#pragma once 
+
+#include <cmath>
+#include <stdexcept>
 
 class Operation {
+protected:
+    char operatorSign;
+
 public:
-    virtual double wykonaj(double a, double b) const = 0;
+    virtual ~Operation() = default;
+    virtual double execute(double a, double b) const = 0;
+    char getOperator() const
+    {
+        return operatorSign;
+    }
+
+    Operation(char sign) : operatorSign(sign) {}
+};
+
+class Addition : public Operation {
+public:
+    double execute(double a, double b) const override {
+        return a + b;
+    }
+
+    Addition() : Operation('+') {}
+};
+
+class Subtraction : public Operation {
+public:
+    double execute(double a, double b) const override {
+        return a - b;
+    }
+
+    Subtraction() : Operation('-') {}
+};
+
+class Multiplication : public Operation {
+public:
+    double execute(double a, double b) const override {
+        return a * b;
+    }
+
+    Multiplication() : Operation('*') {}
+};
+
+class Division : public Operation {
+public:
+    double execute(double a, double b) const override 
+    {
+        if (b == 0) 
+        {
+            throw std::runtime_error("Division by zero");
+        }
+        return a / b;
+    }
+
+    Division() : Operation('/') {}
 };
